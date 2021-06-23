a71dddd74d85f8876700ad3ae17458e5fd8dc7a1
Jhonah <buryakone@gmail.com>
2021-06-23 18:30:20 +0700

    использовал алгоритм Тасование Фишера - Йетса

--- script.js
+++ script.js
@@ -10,19 +10,44 @@ console.log(shuffle(arr));
 // arr = [3, 1, 2]
 
 function shuffle(arr) {
-    //console.log(rand.toFixed(1));
-    return arr.sort( () =>
-        //(Math.random(arr).toFixed(1) >= 0.5) ? 1 : -1
-        randomInteger(1, 4) > 2 ? 1 : -1
-    );
+    for (let i = arr.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        let t = arr[i];
+        arr[i] = arr[j];
+        arr[j] = t;
+    }
+    return arr;
 }
 
-function randomInteger(min, max) {
+/*function randomInteger(min, max) {
     // случайное число от min до (max+1)
 
     let rand = min + Math.random() * (max + 1 - min);
     return Math.floor(rand);
+}*/
+
+/*
+ * подсчёт вероятности для всех возможных вариантов
+ *
+ *
+   let count = {
+    '123': 0,
+    '132': 0,
+    '213': 0,
+    '231': 0,
+    '321': 0,
+    '312': 0
+};
+
+for (let i = 0; i < 1000000; i++) {
+    let array = [1, 2, 3];
+    shuffle(array);
+    count[array.join('')]++;
 }
 
-console.log(Math.random().toFixed(1));
-console.log(randomInteger(1, 3));
+// показать количество всех возможных вариантов
+for (let key in count) {
+    alert(`${key}: ${count[key]}`);
+}
+
+*/
